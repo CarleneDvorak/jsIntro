36f4262993c997da34d6cf518bf754c7fd524ee0
CarleneDvorak <42741569+CarleneDvorak@users.noreply.github.com>
2018-08-30 19:14:16 -0500

    Assignment complete

--- intro.js
+++ intro.js
@@ -1,11 +1,5 @@
-//Celsius to Fahrenheit degrees
+const celsius = 20;
+const fahrenheit = celsius * 9/5 + 32;
 
-// [F] = [C] * 9/5 + 32
-
-console.log (" From Celsius to Fahrenheit degrees");
-
-let yourTemp = Celsius;
-let yourTemp2 = Fahrenheit;
-
-  console.log (yourTemp * 9/5 + 32);
-
+console.log(" your conversion of 20 degree celsius is "
+ + fahrenheit + " Farenheit");
