0549aefaaa73f7f0859d6c11d9f84f947378bbec
CarleneDvorak <42741569+CarleneDvorak@users.noreply.github.com>
2018-09-02 08:32:44 -0500

    Added other two assign. last doesnt work

--- intro.js
+++ intro.js
@@ -3,3 +3,21 @@ const fahrenheit = celsius * 9/5 + 32;
 console.log(" your conversion of 20 degree celsius is "
  + fahrenheit + " Farenheit");
 //Assignment complete
+
+//Times
+const four = 4; 
+//var mult = (1, 2, 3,4, 5, 6, 7, 8, 9, 10);
+//for (var i = 1; i < mult.length, i++;) 
+//console.log(`${four} x ${multiplesOf} = ${four * multiplesOf}`);
+
+var multiplesOf  = 1
+while (multiplesOf  <= 10) console.log(" 4 x "  + four  * multiplesOf++);
+;
+
+//FizzBuss
+console.log("Print all numbers between one and one hundred");
+var fizz= 1;
+var buzz =1;
+while (fizz,buzz <= 100)  { if ( fizz % 3 === 0){ console.log("Fizz ");} 
+else {( buzz % 5 === 0); console.log("buzz");fizz,buzz += 1;
+}}
