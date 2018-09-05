616f33e493f7c33dd0c6a4b6365e849b9e535df3
CarleneDvorak <42741569+CarleneDvorak@users.noreply.github.com>
2018-09-04 20:01:01 -0500

    Added Fizz buzz

--- intro.js
+++ intro.js
@@ -2,22 +2,35 @@ const celsius = 20;
 const fahrenheit = celsius * 9/5 + 32;
 console.log(" your conversion of 20 degree celsius is "
  + fahrenheit + " Farenheit");
-//Assignment complete
+
 
 //Times
 const four = 4; 
-//var mult = (1, 2, 3,4, 5, 6, 7, 8, 9, 10);
-//for (var i = 1; i < mult.length, i++;) 
-//console.log(`${four} x ${multiplesOf} = ${four * multiplesOf}`);
-
 var multiplesOf  = 1
-while (multiplesOf  <= 10) console.log(" 4 x "  + four  * multiplesOf++);
+while (multiplesOf  <= 10) console.log(" 4 x "  + multiplesOf + " = " + (four  * multiplesOf++))
 ;
 
 //FizzBuss
-console.log("Print all numbers between one and one hundred");
-var fizz= 1;
-var buzz =1;
-while (fizz,buzz <= 100)  { if ( fizz % 3 === 0){ console.log("Fizz ");} 
-else {( buzz % 5 === 0); console.log("buzz");fizz,buzz += 1;
-}}
+
+// for loop to iterate numbers 1 - 100
+for (i = 1; i <= 100; i++) {
+
+ // (if statement) is divisible by 3 and 5 = FizzBuzz
+    if (i% 3 === 0 && i % 5=== 0) {
+        console.log("FizzBuzz");
+    }
+
+//else  if= the number is divisible by 3 = Fizz
+    else if (i % 3=== 0) {
+        console.log("Fizz");
+    }
+
+ // if the number is divisible by 5 = Buzz
+    else if (i % 5=== 0) {
+        console.log("Buzz");
+    }
+ // otherwise, list the number alone
+    else {
+        console.log(i);
+    }
+}
